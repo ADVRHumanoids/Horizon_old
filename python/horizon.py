05d578599c78825d3f44f9bc79559a0fd2fe6531
EnricoMingo <enricomingo@gmail.com>
2020-02-03 17:13:45 +0100

    First commit

---
+++ python/horizon.py
@@ -0,0 +1,112 @@
+from casadi import *
+
+
+# create_bounds: creates a list of bounds, along the number of knots, for the variables given the list of bounds
+# on states x and controls u
+def create_bounds(x_min, x_max, u_min, u_max, number_of_nodes):
+    v_min = []
+    v_max = []
+    for k in range(number_of_nodes):
+        v_min += x_min
+        v_max += x_max
+        v_min += u_min
+        v_max += u_max
+    v_min += x_min
+    v_max += x_max
+
+    return v_min, v_max
+
+
+
+# concat_states_and_controls: creates a list concatenating (vertically) each variable concatenated variable contained in X
+# and U at the same node adding the final state at the end:
+# Example: X = [vertcat(Q0, Qdot0), vertcat(Q1, Qdot1), vertcat(Q2, Qdot2), ..., vertcat(Qn, Qdotn)]'
+#          U = [vertcat(Qddot0, F0), vertcat(Qddot1, F1), vertcat(Qddot2, F2), ..., vertcat(Qddotn-1, Fn-1)]'
+# then V = [vertcat(Q0, Qdot0, Qddot0, F0), vertcat(Q1, Qdot1, Qddot1, F1), vertcat(Q2, Qdot2, Qddot2, F2), ..., vertcat(Qn, Qdotn)]'
+def concat_states_and_controls(X, U):
+    ns = np.size(U)
+    V = []
+    for k in range(ns):
+        V.append(vertcat(X[k], U[k]))
+    V.append(X[ns])
+    return V
+
+# concat: creates a list concatenating (vertically) each variable contained in V at the same node:
+# Example: V = [Q, Qdot] then X = [vertcat(Q0, Qdot0), vertcat(Q1, Qdot1), vertcat(Q2, Qdot2), ...]'
+def concat(V, s):
+    X = []
+    for k in range(s):
+        x = []
+        for j in V:
+            x.append(j[k])
+        X.append(vertcat(*x))
+    return X
+
+# create_state_and_control: creates state list X and control list U  from VX list and VU list using concat
+def create_state_and_control(VX, VU):
+    #MISSING CHECK OF SIZE OF VARIABLES CONTAINED IN VX!!!
+    ns = np.size(VX[0])
+    X = concat(VX, ns)
+
+    # MISSING CHECK OF SIZE OF VARIABLES CONTAINED IN VU!!!
+    ns = np.size(VU[0])
+    U = concat(VU, ns)
+    return X, U
+
+# dynamic_model_with_floating_base: gets in input a q of size [n, 1] and a qdot of size [n-1, 1]
+# (notice that the first 7 elements of q are postion and orientation with quaternion)
+# and return the dynamic model x, xdot considering the integration of the quaterion for the floating base orientation
+def dynamic_model_with_floating_base(q, qdot, qddot):
+    # Model equations
+    S = SX.zeros(3, 3)
+    S[0, 1] = -q[5]
+    S[0, 2] = q[4]
+    S[1, 0] = q[5]
+    S[1, 2] = -q[3]
+    S[2, 0] = -q[4]
+    S[2, 1] = q[3]
+
+    # Quaternion Integration
+    tmp1 = casadi.mtimes(0.5 * (q[6] * SX.eye(3) - S), qdot[3:6])
+    tmp2 = -0.5 * casadi.mtimes(q[3:6].T, qdot[3:6])
+
+    x = vertcat(q, qdot)
+    xdot = vertcat(qdot[0:3], tmp1, tmp2, qdot[6:qdot.shape[0]], qddot)
+
+    return x, xdot
+
+# create_variable: return a SX_var of size [size, 1] and a MX_var of size [size, ns] where:
+# if type is STATE then ns = number_of_nodes + 1
+# if type is CONTRON then ns = number_of_nodes
+# if type is FINAL_STATE then ns = 1
+# else ns = 0
+def create_variable(name, size, number_of_nodes, type):
+    SX_var = SX.sym('SX_'+name, size)
+    MX_var = []
+
+    ns = 0
+    if type == "STATE":
+        ns = number_of_nodes + 1
+    elif type == "CONTROL":
+        ns = number_of_nodes
+    elif type == "FINAL_STATE":
+        ns = 1
+
+    for i in range(ns):
+        MX_var.append(MX.sym(name + str(i), SX_var.size1()))
+
+    return SX_var, MX_var
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ python/multiple_shooting_example.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python
+from horizon import *
+import casadi_kin_dyn.pycasadi_kin_dyn as cas_kin_dyn
+import matlogger2.matlogger as matl
+import rospy
+
+logger = matl.MatLogger2('/tmp/template_rope_log')
+logger.setBufferMode(matl.BufferMode.CircularBuffer)
+
+urdf = rospy.get_param('robot_description')
+kindyn = cas_kin_dyn.CasadiKinDyn(urdf)
+
+# Forward Kinematics of interested links
+FK_waist = Function.deserialize(kindyn.fk('Waist'))
+FKR = Function.deserialize(kindyn.fk('Contact1'))
+FKL = Function.deserialize(kindyn.fk('Contact2'))
+FKRope = Function.deserialize(kindyn.fk('rope_anchor2'))
+
+# Inverse Dynamics
+ID = Function.deserialize(kindyn.rnea())
+
+# Jacobians
+Jac_waist = Function.deserialize(kindyn.jacobian('Waist'))
+Jac_CRope = Function.deserialize(kindyn.jacobian('rope_anchor2'))
+
+# Optimization Params
+ns = 30  # number of shooting nodes
+
+nc = 3  # number of contacts
+
+nq = kindyn.nq()  # number of DoFs - NB: 7 DoFs floating base (quaternions)
+
+
+DoF = nq - 7 # Contacts + anchor_rope + rope
+
+nv = kindyn.nv() # Velocity DoFs
+nf = 3*nc # 2 feet contacts + rope contact with wall, Force DOfs
+
+# Variables
+q, Q = create_variable("Q", nq, ns, "STATE")
+q_min = np.array([-10.0, -10.0, -10.0, -1.0, -1.0, -1.0, -1.0, # Floating base
+                  -0.3, -0.1, -0.1, # Contact 1
+                  -0.3, -0.05, -0.1, # Contact 2
+                  -1.57, -1.57, -3.1415, #rope_anchor
+                  0.5]) #rope
+q_max = np.array([10.0,  10.0,  10.0,  1.0,  1.0,  1.0,  1.0, # Floating base
+                  0.3, 0.05,  0.1, # Contact 1
+                  0.3, 0.1,  0.1, # Contact 2
+                  1.57, 1.57, 3.1415,  #rope_anchor
+                  0.5]) #rope
+q_init = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0,
+                   0., 0., 0.,
+                   0., 0., 0.,
+                   0., 0., 0.,
+                   0.5])
+
+qdot, Qdot = create_variable('Qdot', nv, ns, "STATE")
+qdot_min = np.full((1, nv), -100.)
+qdot_max = np.full((1, nv), 100.)
+
+qddot, Qddot = create_variable('Qddot', nv, ns, "CONTROL")
+qddot_min = np.full((1, nv), -100.)
+qddot_max = np.full((1, nv), 100.)
+
+
+f, F = create_variable('F', nf, ns, "CONTROL")
+f_min = np.tile(np.array([-10000., -10000., -10000.]), nc)
+f_max = np.tile(np.array([10000., 10000., 10000.]), nc)
+
+
+x, xdot = dynamic_model_with_floating_base(q, qdot, qddot)
+
+L = 0.01*dot(qddot, qddot) # Objective term
+
+tf = 3. #[s]
+
+#Formulate discrete time dynamics
+dae = {'x': x, 'p': qddot, 'ode': xdot, 'quad': L}
+opts = {'tf': tf/ns}
+F_integrator = integrator('F_integrator', 'rk', dae, opts)
+
+# Start with an empty NLP
+
+X, U = create_state_and_control([Q, Qdot], [Qddot, F])
+V = concat_states_and_controls(X,U)
+
+v_min, v_max = create_bounds([q_min, qdot_min], [q_max, qdot_max], [qddot_min, f_min], [qddot_max, f_max], ns)
+
+# Create Problem (J, v_min, v_max, g_min, g_max)
+g = []
+g_min = []
+g_max = []
+J = MX([0])
+for k in range(ns):
+    CRope_jac = Jac_CRope(q=Q[k])['J']
+    JtF = mtimes(CRope_jac.T, vertcat(F[k][6:9], MX.zeros(3,1)))
+
+    Tau = ID(q=Q[k], v=Qdot[k], a=Qddot[k])['tau'] - JtF
+
+    # Cost function
+
+    J += 1*dot(Q[k], Q[k])
+
+    # Multiple shooting constraint
+    integrator_out = F_integrator(x0=X[k], p=Qddot[k])
+    g += [integrator_out['xf'] - X[k + 1]]
+    g_min += [0] * X[k + 1].size1()
+    g_max += [0] * X[k + 1].size1()
+
+    # Underactuation on spherical joint rope
+    g += [Tau[12:15]]
+    g_min += np.array([0., 0., 0.]).tolist()
+    g_max += np.array([0., 0., 0.]).tolist()
+
+    # Floating base constraint
+    g += [Tau[0:6]]
+    g_min += np.zeros((6, 1)).tolist()
+    g_max += np.zeros((6, 1)).tolist()
+
+    # Contact constraints
+    CRope_pos_init = FKRope(q=q_init)['ee_pos']
+    CRope_pos = FKRope(q=Q[k])['ee_pos']
+    g += [CRope_pos - CRope_pos_init]
+    g_min += np.array([0., 0., 0.]).tolist()
+    g_max += np.array([0., 0., 0.]).tolist()
+
+    # Unilaterality on rope Force
+    g += [Tau[15:16]]
+    g_min += np.array([-10000.]).tolist()
+    g_max += np.array([0.]).tolist()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
