080c1dcc383ba7363d14f4b941a198b0a8802c0c
matteoparigi <matteo.parigi@iit.it>
2020-04-05 17:24:01 +0200

    Added computation of CoM pos vel acc

--- python/utils/kinematics.py
+++ python/utils/kinematics.py
@@ -3,9 +3,11 @@ from horizon import *
 
 
 class kinematics:
-    def __init__(self, kindyn, Q):
+    def __init__(self, kindyn, Q, Qdot, Qddot):
         self.kindyn = kindyn
         self.Q = Q
+        self.Qdot = Qdot
+        self.Qddot = Qddot
 
     def computeFK(self, link_name, fk_type, from_node, to_node):
         FK = Function.deserialize(self.kindyn.fk(link_name))
@@ -21,3 +23,22 @@ class kinematics:
             raise NotImplementedError()
 
         return vertcat(*link_pos)
+
+    def computeCoM(self, fk_type, from_node, to_node):
+        FK = Function.deserialize(self.kindyn.centerOfMass())
+
+        CoM = []
+
+        if fk_type is 'com':
+            for k in range(from_node, to_node):
+                CoM.append(FK(q=self.Q[k])['com'])
+        elif fk_type is 'vcom':
+            for k in range(from_node, to_node):
+                CoM.append(FK(q=self.Q[k], v=self.Qdot[k])['vcom'])
+        elif fk_type is 'acom':
+            for k in range(from_node, to_node):
+                CoM.append(FK(q=self.Q[k], v=self.Qdot[k], a=self.Qddot[k])['acom'])
+        else:
+            raise NotImplementedError()
+
+        return vertcat(*CoM)
