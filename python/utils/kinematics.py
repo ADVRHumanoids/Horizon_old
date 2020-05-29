ecdd15e22e01f4f7a7d0487b24b1cca1d9334a7c
EnricoMingo <enricomingo@gmail.com>
2020-05-29 10:31:25 +0200

    added documantation (partially)

--- python/utils/kinematics.py
+++ python/utils/kinematics.py
@@ -2,13 +2,40 @@ from casadi import *
 from horizon import *
 
 class kinematics:
+    """
+    The kinematics class provides easy accessor to computation of FK quantities such as Cartesian poses and veocities
+
+    """
     def __init__(self, kindyn, Q, Qdot, Qddot):
+        """
+        Constructor
+        Args:
+            kindyn: casadi_kin_dyn object
+            Q: state positions
+            Qdot: state velocities
+            Qddot: control accelerations
+        """
         self.kindyn = kindyn
         self.Q = Q
         self.Qdot = Qdot
         self.Qddot = Qddot
 
     def computeKineticEnergy(self, from_node, to_node):
+        """
+        Computes kinetic energy of the system:
+
+            0.5*m*v^2
+
+        for a given interval of nodes.
+
+        Args:
+            from_node: starting node
+            to_node: end node
+
+        Returns:
+            vertcat(*DT) of kinetic energies
+
+        """
         KineticEnergy = Function.deserialize(self.kindyn.kineticEnergy())
         DT = []
         for k in range(from_node, to_node):
@@ -16,6 +43,21 @@ class kinematics:
         return vertcat(*DT)
 
     def computePotentialEnergy(self, from_node, to_node):
+        """
+        Computes kinetic energy of the system:
+
+            m*g*h
+
+        for a given interval of nodes.
+
+        Args:
+            from_node: starting node
+            to_node: end node
+
+        Returns:
+            vertcat(*DT) of potential energies
+
+        """
         PotentialEnergy = Function.deserialize(self.kindyn.potentialEnergy())
         DU = []
         for k in range(from_node, to_node):
@@ -23,6 +65,25 @@ class kinematics:
         return vertcat(*DU)
 
     def computeDiffFK(self, link_name, fk_type, ref, from_node, to_node):
+        """
+        Computes differential forward kinematics quantities: velocities and Jacobians ///TODO: Add accelerations
+        for a given number of nodes.
+        Args:
+            link_name: frame wrt computes quantities
+            fk_type:
+                'ee_vel_linear' for linear velocity
+                'ee_vel_angular' for angular velocity
+                'ee_jacobian' for jacobian
+            ref: this is the frame which is used to compute the Jacobian during the ID computation:
+                LOCAL
+                WORLD
+                LOCAL_WORLD_ALIGNED
+            from_node: start node
+            to_node: end node
+
+        Returns:
+
+        """
         FK_vel = Function.deserialize(self.kindyn.frameVelocity(link_name, ref))
         # FK_acc = Function.deserialize(self.kindyn.frameAcceleration(link_name))
         Jac = Function.deserialize(self.kindyn.jacobian(link_name, ref))
