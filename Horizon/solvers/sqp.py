c4bf8e36106052f6ff27e7376ff555b3f198e0ec
EnricoMingo <enricomingo@gmail.com>
2020-10-12 19:00:57 +0200

    fix to use sqp without contraints (only bounds). Small performance improvements in this case...

--- Horizon/solvers/sqp.py
+++ Horizon/solvers/sqp.py
@@ -52,7 +52,10 @@ class sqp(object):
         self.__qpsolver_options = self.qpsolver_option_parser(self.__qpsolver, self.__options_dict)
 
         self.__f = self.__problem_dict['f']
-        self.__g = self.__problem_dict['g']
+        self.__g = None
+        if 'g' in self.__problem_dict:
+            self.__g = self.__problem_dict['g']
+
         self.__x = self.__problem_dict['x']
 
         self.__max_iter = 1000
@@ -70,11 +73,15 @@ class sqp(object):
         self.__r_fcn = Function('r_fcn', {'v': self.__x, 'r': self.__f}, ['v'], ['r'])
 
         # Form function for calculating the constraints
-        self.__g_fcn = Function('g_fcn', {'v': self.__x, 'g': self.__g}, ['v'], ['g'])
+        self.__g_fcn = []
+        if self.__g is not None:
+            self.__g_fcn = Function('g_fcn', {'v': self.__x, 'g': self.__g}, ['v'], ['g'])
 
         # Generate functions for the Jacobians
         self.__Jac_r_fcn = self.__r_fcn.jac()
-        self.__Jac_g_fcn = self.__g_fcn.jac()
+        self.__Jac_g_fcn = []
+        if self.__g is not None:
+            self.__Jac_g_fcn = self.__g_fcn.jac()
 
         self.__v0 = []
         self.__vmin = []
@@ -108,18 +115,20 @@ class sqp(object):
         if init:
             # QP structure
             self.__qp['h'] = H.sparsity()
-            self.__qp['a'] = A.sparsity()
-
+            if A is not None:
+                self.__qp['a'] = A.sparsity()
 
             # Create CasADi solver instance
             self.__solver = conic('S', self.__qpsolver, self.__qp, self.__qpsolver_options)
 
+        if A is None:
+            A = []
         r = self.__solver(h=H, g=g, a=A, lbx=lbx, ubx=ubx, lba=lba, uba=uba)
 
         # Return the solution
         return r['x']
 
-    def __call__(self, x0, lbx, ubx, lbg, ubg):
+    def __call__(self, x0, lbx, ubx, lbg = [], ubg = []):
         """
         Compute solution of non linear problem
         Args:
@@ -153,9 +162,13 @@ class sqp(object):
             r_k = self.__r_fcn(v=self.__v_opt)['r']
 
             # Form quadratic approximation of constraints
-            Jac_g_fcn_value = self.__Jac_g_fcn(v=self.__v_opt)  # evaluate in v_opt
-            J_g_k = Jac_g_fcn_value['DgDv']
-            g_k = self.__g_fcn(v=self.__v_opt)['g']
+            Jac_g_fcn_value = []
+            J_g_k = None
+            g_k = []
+            if self.__g is not None:
+                Jac_g_fcn_value = self.__Jac_g_fcn(v=self.__v_opt)  # evaluate in v_opt
+                J_g_k = Jac_g_fcn_value['DgDv']
+                g_k = -self.__g_fcn(v=self.__v_opt)['g']
 
             # Gauss-Newton Hessian
             H_k = mtimes(J_r_k.T, J_r_k)
@@ -168,12 +181,14 @@ class sqp(object):
             dv_max = self.__vmax - self.__v_opt
 
             # Solve the QP
-            dv = self.qpsolve(H_k, Grad_obj_k, dv_min, dv_max, J_g_k, -g_k, -g_k, init)
+            dv = self.qpsolve(H_k, Grad_obj_k, dv_min, dv_max, J_g_k, g_k, g_k, init)
 
             # Take the full step
             self.__v_opt += 0.5*dv.toarray().flatten()
             self.__obj.append(float(dot(r_k.T, r_k) / 2.))
-            self.__constr.append(float(norm_2(g_k)))
+            self.__constr = []
+            if self.__g is not None:
+                self.__constr.append(float(norm_2(g_k)))
 
         solution_dict = {'x': self.__v_opt, 'f': self.__obj, 'g': self.__constr}
         return solution_dict
@@ -193,9 +208,13 @@ class sqp(object):
 
         plt.title("SQP solver output")
         plt.semilogy(self.__obj)
-        plt.semilogy(self.__constr)
+        if self.__constr:
+            plt.semilogy(self.__constr)
         plt.xlabel('iteration')
-        plt.legend(['Objective value', 'Constraint violation'], loc='center right')
+        if self.__constr:
+            plt.legend(['Objective value', 'Constraint violation'], loc='center right')
+        else:
+            plt.legend(['Objective value'], loc='center right')
         plt.grid()
 
         plt.show()
