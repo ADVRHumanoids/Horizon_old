0a4774e9562e8f3b8e2b31fa04e9c542ed20fc8c
EnricoMingo <enricomingo@gmail.com>
2020-10-13 15:11:48 +0200

    fixed installation (now the ibrary is imported as Horizon. )

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['Horizon', 'constraints', 'solvers', 'utils'],
+    packages=['Horizon', 'Horizon.constraints', 'Horizon.solvers', 'Horizon.utils'],
     package_dir={'Horizon': 'Horizon',
-		 'constraints': 'Horizon/constraints',
-		 'solvers': 'Horizon/solvers',
-		 'utils': 'Horizon/utils'}
+		 'Horizon.constraints': 'Horizon/constraints',
+		 'Horizon.solvers': 'Horizon/solvers',
+		 'Horizon.utils': 'Horizon/utils'}
 )
 
 setup(**d)
